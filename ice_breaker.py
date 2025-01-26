26710435f37f868b6d3c2fdb40386fd66ece1a5b
Stanislav Deviatov <devyatov@gmail.com>
2025-01-26 18:29:37 +0100

    second task

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -26,10 +28,11 @@ if __name__ == "__main__":
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    llm = ChatOllama(model="llama3.2")
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | StrOutputParser()
 
     res = chain.invoke(input={"information": information})
 
-    print(res.content)
+    print(res)
